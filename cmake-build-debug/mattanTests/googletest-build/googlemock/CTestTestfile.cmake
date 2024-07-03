c6e292dd5db9f62c48ba55bc7ee6d6803b79c84f
nirf808 <nirfin8@gmail.com>
2024-07-03 17:37:38 +0300

    passed all tests

--- cmake-build-debug/mattanTests/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/nirfi/CLionProjects/OS-NEW/EX3/cmake-build-debug/mattanTests/googletest-src/googlemock
-# Build directory: /mnt/c/Users/nirfi/CLionProjects/OS-NEW/EX3/cmake-build-debug/mattanTests/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
